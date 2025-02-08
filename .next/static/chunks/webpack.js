92fe75846dce14ab32eba177ca51bf13ddfcacbb
Nanditha Chevula <nc42756@uga.edu>
2025-02-08 18:07:32 -0500

    Backend structure

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a4d6581017deed6e"; }
+/******/ 		__webpack_require__.h = function() { return "a20de63f4a67081f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
